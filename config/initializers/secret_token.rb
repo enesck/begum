f02504e4163b784ebe3d3200a9e2daca97e3d9a8
enesck <kilicenescan@gmail.com>
2013-08-12 20:00:49 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Begum::Application.config.secret_key_base = '748f51ab38ca753f3998957ec73a3afffe7807dbe962371c8dafd5212b6029f00861d81cea754228868e8dc5a5d27ed9d862049bf575c45aa62bc2133840df1c'
